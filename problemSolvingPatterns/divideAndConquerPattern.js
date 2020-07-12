a8924442abc8a1a79cd1aef5553d9628b2e55f1c
Amneet Sandhu <amneet954@gmail.com>
2020-07-12 17:42:24 -0400

    Added divideAndConquerPattern.js

--- problemSolvingPatterns/divideAndConquerPattern.js
+++ problemSolvingPatterns/divideAndConquerPattern.js
@@ -1,2 +1,55 @@
 //This problem solving method is done by dividing a data set into small pieces and from there, repeating the process with a subset data.
 // This pattern is VERY useful as it SIGNIFICANTLY decreases time complexity.
+
+//Problem: We have an array of sorted integers. Create a function that accepts the second argument value and returns the index of where the value is found, within the array. If the value is not in the array at all, return -1
+
+const divideAndConquer = (array, number) => {
+  if (array.length < 1) return -1;
+
+  let end = array.length - 1;
+  let middle = Math.ceil(end / 2);
+
+  if (array[middle] === number) return middle;
+  else if (array[middle] < number) {
+    let a = middle;
+    while (array[a] < number) a++;
+    if (array[a] !== number) return -1;
+    else return a;
+  } else if (array[middle] > number) {
+    let a = 0;
+    while (array[middle] > number) a++;
+    if (array[a] !== number) return -1;
+    else return a;
+  } else {
+    return -1;
+  }
+};
+
+//Alternate Solution
+
+// const divideAndConquer = (array, number) => {
+//   if (array.length < 1) return -1;
+
+//   let end = array.length - 1;
+//   let middle = Math.ceil(end / 2);
+
+//   if (array[middle] === number) return middle;
+//   else if (array[middle] < number) {
+//     let a = middle;
+//     while (array[a] < number) a++;
+//     if (array[a] !== number) return -1;
+//     else return a;
+//   } else if (array[middle] > number) {
+//     let a = 0;
+//     while (array[middle] > number) a++;
+//     if (array[a] !== number) return -1;
+//     else return a;
+//   } else {
+//     return -1;
+//   }
+// };
+
+console.log(`Answer is: ${divideAndConquer([1, 2, 3, 4, 5, 6], 4)}`); //3
+console.log(`Answer is: ${divideAndConquer([1, 2, 3, 4, 5, 6], 6)}`); //5
+console.log(`Answer is: ${divideAndConquer([1, 2, 3, 4, 5, 6], 11)}`); //-1
+console.log(`Answer is: ${divideAndConquer([11], 11)}`); //0
