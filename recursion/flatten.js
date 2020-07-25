e11a40e37a012b6512739f2faaba46aff77892c1
Amneet Sandhu <amneet954@gmail.com>
2020-07-25 19:49:15 -0400

    Grammar

--- recursion/flatten.js
+++ recursion/flatten.js
@@ -1,4 +1,4 @@
-//Write a functinon that takes a multidimensional sum array and flattens it. Leaving only values in the array, each value in the array should not be an array with value nested in it.
+//Write a function that takes a multidimensional sum array and flattens it. Leaving only values in the array, each value in the array should not be an array with value nested in it.
 
 const flatten = (array) => {
   let final = [];
