53f1fd5b5a7de423ff1dfc69f0b49af1107e3882
Amneet Sandhu <amneet954@gmail.com>
2020-07-27 12:23:35 -0400

    Fixed syntax issue on capitalizeFirst.js

--- recursion/capitalizeFirst.js
+++ recursion/capitalizeFirst.js
@@ -1,11 +1,13 @@
 //Write a function that takes the first letter of each string in the array, and capitalizes it. Assume that all string values in the array are in lower case.
 
 const capitalizeFirst = (array) => {
-  if (array.length === 1)
+  if (array.length === 1) {
     return (array[0] = array[0][0].toUpperCase().concat(array[0].slice(1)));
+  }
 
   array[0] = array[0][0].toUpperCase().concat(array[0].slice(1));
   return array[0].split(" ").concat(capitalizeFirst(array.slice(1)));
 };
 
-capitalizeFirst(["batman", "robin", "nightwing"]);
+// capitalizeFirst(["batman", "robin", "nightwing"]);
+console.log(`Answer: ${capitalizeFirst(["batman", "robin", "nightwing"])}`);
