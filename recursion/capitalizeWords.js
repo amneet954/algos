98fae051c5781679d0d3996f96a7f03c070aacd2
Amneet Sandhu <amneet954@gmail.com>
2020-07-27 12:31:37 -0400

    Added capitalizeWords.js

---
+++ recursion/capitalizeWords.js
@@ -0,0 +1,13 @@
+//Write a function that capitalizes all letters of the string values within the array
+
+const capitalizeWords = (array) => {
+  if (array.length === 1) {
+    return (array[0] = array[0].toUpperCase());
+  }
+
+  array[0] = array[0].toUpperCase();
+  return array[0].split(" ").concat(capitalizeWords(array.slice(1)));
+};
+
+// capitalizeWords(["i", "am", "learning", "recursion"]);
+console.log(`Answer: ${capitalizeWords(["i", "am", "learning", "recursion"])}`);
