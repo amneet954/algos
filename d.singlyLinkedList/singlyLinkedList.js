5245f606a4b418aa7e6d11bdeccca4d9fdc20833
Amneet Sandhu <amneet954@gmail.com>
2020-08-22 19:45:42 -0400

    Added unshift and shift into singlyLinkedList.js

--- d.singlyLinkedList/singlyLinkedList.js
+++ d.singlyLinkedList/singlyLinkedList.js
@@ -19,7 +19,51 @@ class SinglyLinkedList {
       this.tail = this.head;
     } else {
       this.tail.next = newNode;
-      this.tail = newNode;
+      this.tail = this.tail.next;
+    }
+    this.length += 1;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return this;
+    let current = this.head;
+    let previousNode = current;
+    while (current.next) {
+      previousNode = current;
+      current = current.next;
+    }
+    this.tail = previousNode;
+    console.log(this.tail);
+    this.tail.next = null;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return this;
+  }
+
+  shift() {
+    if (!this.head) return this;
+    this.head = this.head.next;
+    this.length -= 1;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return this;
+  }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      let currentHead = this.head;
+      this.head = newNode;
+      this.head.next = currentHead;
     }
     this.length += 1;
     return this;
@@ -29,4 +73,8 @@ class SinglyLinkedList {
 let list = new SinglyLinkedList();
 list.push("Hello");
 list.push("Bye");
+console.log(list);
+
+list.pop();
+
 console.log(list);
